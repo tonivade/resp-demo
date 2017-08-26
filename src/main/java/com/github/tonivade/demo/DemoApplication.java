ba676764e9c9abef26cb02d3ed3370d5c6bbf404
Antonio Muñoz <antonio.munoz@rumbo.com>
2017-08-26 18:07:58 +0200

    updated demo with latest version of resp-server and spring boot 2.0.0.M3

--- src/main/java/com/github/tonivade/demo/DemoApplication.java
+++ src/main/java/com/github/tonivade/demo/DemoApplication.java
@@ -2,27 +2,10 @@ package com.github.tonivade.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.github.tonivade.demo.command.CreateUserCommand;
-import com.github.tonivade.demo.command.GetAllUsersCommand;
-import com.github.tonivade.demo.command.GetUserCommand;
-import com.github.tonivade.resp.command.CommandSuite;
-import com.github.tonivade.resp.command.CommandWrapperFactory;
 
 @SpringBootApplication
 public class DemoApplication
 {
-  @Bean
-  public CommandSuite commandSuite(CommandWrapperFactory factory)
-  {
-    return new CommandSuite(factory) {{
-      addCommand(GetUserCommand.class);
-      addCommand(CreateUserCommand.class);
-      addCommand(GetAllUsersCommand.class);
-    }};
-  }
-
   public static void main(String[] args)
   {
     SpringApplication.run(DemoApplication.class, args);
